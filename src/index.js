5be7ed3849fc5d01ec4f6c9ae8a4fab8bc9508cf
Kricharith <jack12688jack@gmail.com>
2024-07-21 05:26:31 +0700

    op

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode >
-        <HashRouter basename='/Kridcharid/profile'>
+        <HashRouter basename='/Kridcharid'>
             <App />
         </HashRouter>
     </React.StrictMode>,
