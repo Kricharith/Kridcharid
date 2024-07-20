fa5a9ca32fa342550d9251323cf74bdef4a7e333
Kricharith <jack12688jack@gmail.com>
2024-07-21 05:10:41 +0700

    upd

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,11 +8,11 @@ import Contact from './pages/Contact';
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="https://kricharith.github.io/Kridcharid/#/" element={<Navigate to="https://kricharith.github.io/Kridcharid/#/profile" replace />} />
-            <Route path="https://kricharith.github.io/Kridcharid/#/profile" element={<Profile />} />
-            <Route path="https://kricharith.github.io/Kridcharid/#/activities" element={<Activities />} />
-            <Route path="https://kricharith.github.io/Kridcharid/#/certificate" element={<Certificate />} />
-            <Route path="https://kricharith.github.io/Kridcharid/#/contact" element={<Contact />} />
+            <Route path="/" element={<Navigate to="/profile" replace />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/certificate" element={<Certificate />} />
+            <Route path="/contact" element={<Contact />} />
         </Routes>
     );
 };
